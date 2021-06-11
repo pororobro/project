b3dfb97d41fdac5cbef69bb5a48d5cb4ff308149
pororobro <pororobro@naver.com>
2021-06-11 16:26:46 +0900

    home updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Login from './user/Login';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Login/>
-    </BrowserRouter>
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
